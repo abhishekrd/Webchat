88b9da0e21c107f74e7f1e7f74b8699d0ee3daf4
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-07-17 16:19:49 +0530

    fix: render loop

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import {Navigate,Outlet} from 'react-router-dom'
-
-const user = localStorage.getItem('user');
+import React, { useEffect, useState } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedRoute = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [user, _setUser] = useState(localStorage.getItem('user'))
   return (
-
-   user ? <Outlet /> : <Navigate to="/login"/>
+    user ? <Outlet /> : <Navigate to="/login" />
   )
 }
 
